2a2b0f7a769fa88a2c559ccccb54bcf69fc2bd79
crydestaahl <kristoffer@oholics.com>
2022-01-31 09:28:19 +0100

    Updates

---
+++ precache-manifest.2a1db683fc4dd6d8ba6b55e643b06d6c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7310f619c802a2155f5f4eda035f10de",
+    "url": "/home-economy-equalizer/index.html"
+  },
+  {
+    "revision": "51c2f387f31e57b28bb1",
+    "url": "/home-economy-equalizer/static/css/4.31c71989.chunk.css"
+  },
+  {
+    "revision": "3f7668486175027dc3df",
+    "url": "/home-economy-equalizer/static/css/main.cc34d9fc.chunk.css"
+  },
+  {
+    "revision": "a829f687bdad4a3f17e0",
+    "url": "/home-economy-equalizer/static/js/3.d9bbdc27.chunk.js"
+  },
+  {
+    "revision": "51c2f387f31e57b28bb1",
+    "url": "/home-economy-equalizer/static/js/4.25b869db.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/home-economy-equalizer/static/js/4.25b869db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c853c92cc3f71c2062e",
+    "url": "/home-economy-equalizer/static/js/5.e37b9085.chunk.js"
+  },
+  {
+    "revision": "c47fb89f944fc413937f1d857df6495a",
+    "url": "/home-economy-equalizer/static/js/5.e37b9085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d6ebbc4035343c067b",
+    "url": "/home-economy-equalizer/static/js/6.2a19b5c1.chunk.js"
+  },
+  {
+    "revision": "f475ec6a22369759e200929069e6ddfa",
+    "url": "/home-economy-equalizer/static/js/6.2a19b5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7668486175027dc3df",
+    "url": "/home-economy-equalizer/static/js/main.7ff4dc26.chunk.js"
+  },
+  {
+    "revision": "95a382b523e114e2ccda",
+    "url": "/home-economy-equalizer/static/js/runtime-main.04154ea2.js"
+  },
+  {
+    "revision": "520e7c17ce67d22d6d32",
+    "url": "/home-economy-equalizer/static/js/xlsx.11005b29.chunk.js"
+  }
+]);
---
+++ static/js/xlsx.11005b29.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonphome-economy"]=this["webpackJsonphome-economy"]||[]).push([[2],{238:function(o,n){},325:function(o,n){},326:function(o,n){}}]);
+//# sourceMappingURL=xlsx.11005b29.chunk.js.map
